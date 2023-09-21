c43163edfe6c3059372048f3d2029625357f3883
Saqib Shah <xaqibshah@gmail.com>
2023-09-21 10:05:23 +0500

    start learning C and work done with scanf

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main (){
+    // int age = 15;
+    int age;
+    // char fname = 'S';
+    
+    // float pi = 3.14;
+    // printf("kch b likh do \n");
+    // printf("kch b likh do \n");
+    // printf("kch b likh do \n");
+    // printf("kch b likh do \n");
+    // printf("kch b likh do \n");
+
+    // printf("age is %d", age);
+    // printf("age is %c", fname);
+    
+    printf("Enter your age ");
+    scanf("%d", &age);
+    printf("your age is %d", age);
+    return 0;
+}
