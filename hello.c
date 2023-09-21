67076edcc3f83cd9c67ab7f72869af573ea6276c
Saqib Shah <xaqibshah@gmail.com>
2023-09-21 10:18:19 +0500

    work with int and create hard code sum program

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main (){
     // int age = 15;
-    int age;
+    // int age;
     // char fname = 'S';
     
     // float pi = 3.14;
@@ -15,8 +15,25 @@ int main (){
     // printf("age is %d", age);
     // printf("age is %c", fname);
     
-    printf("Enter your age ");
-    scanf("%d", &age);
-    printf("your age is %d", age);
+    // printf("Enter your age ");
+    // scanf("%d", &age);
+    // printf("your age is %d", age);
+   
+   int num1, num2;
+
+   printf("Enter first Number: \n");
+   scanf("%d", &num1);
+   printf(" Enter second number :\n");
+   scanf("%d",&num2 );
+   int sum = num1 + num2;
+   printf("sum is %d", sum);
+
+//    printf("your first num is: %d ", num1, "and second num is: %d", num2);
+//    printf("number1 + number2 is: &d", num1+num2);
+   
+   
+   
+   
+   
     return 0;
 }
