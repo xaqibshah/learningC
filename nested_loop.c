cb0c7c9e79697b527b32a0d0e283cec03e4e3974
Saqib Shah <saqibshah@live.com>
2023-10-25 06:56:17 +0500

    worked with loops

---
+++ nested_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n = 4;
+    int i,j;
+
+    for (i = 0; i < n; i++)
+    {   
+        printf("%d ", i);
+        for( j=1 ; j< n; j++){
+            printf("%d \n",j);
+
+
+
+            /* code */
+        }
+    }
+
+    
+}
